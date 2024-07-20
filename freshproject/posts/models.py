4f4d897ce1ec985b67965e104c5ce08f6063b570
betiniakarandut <betiniakarandut@gmail.com>
2024-07-20 21:43:03 +0100

    feat: post page

--- freshproject/posts/models.py
+++ freshproject/posts/models.py
@@ -5,7 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
     slug = models.SlugField()
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='fallback.png', blank=True)
 
     def __str__(self):
         return self.title
